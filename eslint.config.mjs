1d818661f1446998a2450aa19e5b449de7b57249
Ian Channing <ian_channing@hotmail.com>
2025-06-09 19:37:52 +0200

    build: eslint pretter config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURL_to_path(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
@@ -47,4 +47,3 @@ export default defineConfig([
   // any conflicting formatting rules from previous configurations.
   prettierConfig,
 ]);
-
